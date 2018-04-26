ae2447615331f34f73184ee212d55c5b98e28f20
Alexander Brand <alexbrand09@gmail.com>
2018-04-26 17:18:56 -0400

    discovery: refactor service name shortening code
    Signed-off-by: Alexander Brand <alexbrand09@gmail.com>

--- discovery/pkg/translator/translator.go
+++ discovery/pkg/translator/translator.go
@@ -23,8 +23,8 @@ const (
 	// GimbalLabelCluster is the key of the label that contains the cluster name
 	GimbalLabelCluster = "gimbal.heptio.com/cluster"
 	gimbalLabelService = "gimbal.heptio.com/service"
-	// Services in Kubernetes are limited to 63 characters in length
-	maxNameLength = 63
+	// https://github.com/kubernetes/community/blob/master/contributors/design-proposals/architecture/identifiers.md
+	kubernetesDNSLabelMaxLength = 63
 )
 
 // AddGimbalLabels returns a new set of labels that includes the incoming set of
@@ -44,16 +44,36 @@ func AddGimbalLabels(clustername, namespace, name string, existingLabels map[str
 	return existingLabels
 }
 
-// GetFormattedName take the names of a service and formats to truncate if needed
-func GetFormattedName(name, cluster string) string {
-	return hashname(maxNameLength, name, cluster)
+// BuildKubernetesDNSLabel joins and lowercases the incoming strings into a
+// single string using "-" as a separator, making sure the resulting string
+// conforms to the Kubernetes DNS Label specification:
+//
+// "An alphanumeric (a-z, and 0-9) string, with a maximum length of 63
+// characters, with the '-' character allowed anywhere except the first or last
+// character, suitable for use as a hostname or segment in a domain name."
+// (https://github.com/kubernetes/community/blob/master/contributors/design-proposals/architecture/identifiers.md)
+//
+// If the combined length exceeds 63 characters, each string in s will be
+// shortened if its length is greater than 63/len(s) using a hash derived from
+// the contents of s (not the current element). In other words, each string in s
+// gets alloted an equal maximum size that it can fill, and is shortened in the
+// case that it goes over the maximum length.
+//
+// The shortening process starts from the last element in s, and will continue
+// shortening each element of s until the resulting string does not exceed 63
+// characters. If all elements are shortened, and the final string is still
+// greater than 63 characters, the entire string is replaced with the hash of
+// the contents of s.
+func BuildKubernetesDNSLabel(s ...string) string {
+	joined := hashname(kubernetesDNSLabelMaxLength, s...)
+	return strings.ToLower(joined)
 }
 
 // hashname takes a lenth l and a varargs of strings s and returns a string whose length
-// which does not exceed l. Internally s is joined with strings.Join(s, "/"). If the
-// combined length exceeds l then hashname truncates each element in s, starting from the
+// which does not exceed l. Internally s is joined with strings.Join(s, "-"). If the
+// combined length exceeds l then hashname shortens each element in s, starting from the
 // end using a hash derived from the contents of s (not the current element). This process
-// continues until the length of s does not exceed l, or all elements have been truncated.
+// continues until the length of s does not exceed l, or all elements have been shortened.
 // In which case, the entire string is replaced with a hash not exceeding the length of l.
 func hashname(l int, s ...string) string {
 	const shorthash = 6 // the length of the shorthash
@@ -65,7 +85,7 @@ func hashname(l int, s ...string) string {
 	}
 	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(r)))
 	for n := len(s) - 1; n >= 0; n-- {
-		s[n] = truncate(l/len(s), s[n], hash[:shorthash])
+		s[n] = shorten(l/len(s), s[n], hash[:shorthash])
 		r = strings.Join(s, "-")
 		if l > len(r) {
 			return r
@@ -76,9 +96,9 @@ func hashname(l int, s ...string) string {
 	return hash[:min(len(hash), l)]
 }
 
-// truncate truncates s to l length by replacing the
+// shorten shortens s to l length by replacing the
 // end of s with -suffix.
-func truncate(l int, s, suffix string) string {
+func shorten(l int, s, suffix string) string {
 	if l >= len(s) {
 		// under the limit, nothing to do
 		return s
